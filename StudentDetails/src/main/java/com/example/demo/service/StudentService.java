c11b1b7b8ecdda67c6836b021bcf410a265c3cef
vaish <vaish@LAPTOP-DFKRDQIJ>
2023-05-28 09:00:47 +0530

    git commit

---
+++ StudentDetails/src/main/java/com/example/demo/service/StudentService.java
@@ -0,0 +1,12 @@
+
+package com.example.demo.service;
+
+import com.example.demo.module.Student;
+
+public interface StudentService  {
+
+	Student addStudent(Student s);
+	
+	Student findStd(Integer studentId);
+}
+
